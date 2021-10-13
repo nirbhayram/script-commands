f01733b4becce08a809ea4c44e1622b7c5d1eee9
Nirbhay Ram <nirbhay.ram@lyearn.com>
2021-10-13 14:05:49 +0530

    chore: new scripts added with generic scripts folder

---
+++ generic_scripts/clear_pods.sh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+
+cd ~/WebstormProjects/frontend-universe/packages/apps/classroom/ios || exit
+echo "========================================================"
+echo "removing pods"
+echo "========================================================"
+
+rm -rf Pods/
